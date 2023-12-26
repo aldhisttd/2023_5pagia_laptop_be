e10f2ff68f958c7fe63a6a651bfc5a07643ea164
yesayas17 <yossiyesayas@gmail.com>
2023-12-26 14:40:27 +0700

    B06 Input Merek (Create)-Revisi

---
+++ merek_create.php
@@ -0,0 +1,31 @@
+<?php
+include "env.php";
+
+$res = [
+ "status" => 200,
+ "msg" => "",
+ "body" => [
+      "data" => [
+          "nama" => "",
+      ]
+ ]
+      ];
+$kode = $_POST['kode'];
+$nama = $_POST['nama'];
+$s = mysqli_query($koneksi, "INSERT INTO merek(kode, nama) VALUES ('$kode', '$nama')");
+
+if ($s) {
+
+    $res['status'] = 200;
+    $res['msg'] = "Data berhasil diinsert";
+    $res['body'] = "";
+   } else {
+    $res['status'] = 400;
+    $res['msg'] = "Gagal membuat merek";
+    $res['body'] = "";
+   }
+   
+   echo json_encode($res);
+   ?>
+
+   
