a8378279e20573724b4e7532437261a87462f8ce
yesayas17 <yossiyesayas@gmail.com>
2023-12-26 20:47:14 +0700

    B14 API Delete Laptop (Delete)

--- laptop_delete.php
+++ laptop_delete.php
@@ -1,26 +1,37 @@
-<?php 
+<?php
 include "env.php";
-
 $res = [
-  "status" => 200,
-  "msg" => "",
-  "body" => "",
+    "status" => 200,
+    "msg" => "",
+    "body" => "",
 ];
 
-$kode = $_GET['kode'];
+$kode = mysqli_real_escape_string($koneksi, $_REQUEST['kode']);
 
-$s = mysqli_query($koneksi, "DELETE FROM laptop WHERE kode='$kode'");
+$d = mysqli_query($koneksi, "SELECT kode, gambar FROM laptop WHERE kode='$kode'");
+$dt = mysqli_fetch_array($d);
 
-if ($s){
-  $res['status'] = 200;
-  $res['msg'] = "Data berhasil dihapus";
-  $res['body'] = "";
+if ($dt) {
+    if (isset($dt['gambar']) && !empty($dt['gambar'])) {
+        $gambar = $dt['gambar'];
+        unlink('gambar/' . $gambar);
+    }
+
+    $q = mysqli_query($koneksi, "DELETE FROM laptop WHERE kode='$kode'");
+    if ($q) {
+        $res['status'] = 200;
+        $res['msg'] = "Data berhasil dihapus";
+        $res['body'] = "";
+    } else {
+        $res['status'] = 404;
+        $res['msg'] = "Data tidak ditemukan";
+        $res['body'] = "";
+    }
 } else {
-  $res['status'] = 400;
-  $res['msg'] = "Data gagal dihapus";
-  $res['body'] = "";
+    $res['status'] = 400;
+    $res['msg'] = "Data gagal dihapus";
+    $res['body'] = "";
 }
 
 echo json_encode($res);
-
 ?>
