b2e9aeb6aaa74f17f74e95f05eafdc9096ae4cea
yesayas17 <yossiyesayas@gmail.com>
2023-12-27 14:41:19 +0700

    mengganti nama folder gambar menjadi upload

--- laptop_delete.php
+++ laptop_delete.php
@@ -1,9 +1,12 @@
 <?php
 include "env.php";
+
 $res = [
-    "status" => 200,
-    "msg" => "",
-    "body" => "",
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
 ];
 
 $kode = mysqli_real_escape_string($koneksi, $_REQUEST['kode']);
@@ -14,19 +17,19 @@ $dt = mysqli_fetch_array($d);
 if ($dt) {
     if (isset($dt['gambar']) && !empty($dt['gambar'])) {
         $gambar = $dt['gambar'];
-        unlink('gambar/' . $gambar);
+        unlink('upload/' . $gambar);
     }
 
     $q = mysqli_query($koneksi, "DELETE FROM laptop WHERE kode='$kode'");
     if ($q) {
         $res['status'] = 200;
         $res['msg'] = "Data berhasil dihapus";
-        $res['body'] = "";
+        $res['body']['data']['kode'] = "$kode";
     } 
 } else {
     $res['status'] = 400;
     $res['msg'] = "Data gagal dihapus";
-    $res['body'] = "";
+    $res['body']['data']['kode'] = "$kode";
 }
 
 echo json_encode($res);
