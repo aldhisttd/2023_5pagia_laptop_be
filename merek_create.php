139f2ac1cc6213a25e4a6ff2660f2a58848340e3
yesayas17 <yossiyesayas@gmail.com>
2023-12-27 14:54:04 +0700

    Respon sama dengan API Spec

--- merek_create.php
+++ merek_create.php
@@ -1,28 +1,30 @@
 <?php
 include "env.php";
 
-$res = [
-  "status" => 200,
-  "msg" => "",
-  "body" => [
-      "data" => [
-          "nama" => "",
-      ]
-  ]
-      ];
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
 $kode = $_POST['kode'];
 $nama = $_POST['nama'];
+
 $s = mysqli_query($koneksi, "INSERT INTO merek (kode, nama) VALUES ('$kode', '$nama')");
 
-if ($s) {
 
-  $res['status'] = 200;
-  $res['msg'] = "Data berhasil diinsert";
-  $res['body']="";
+if ($s) {
+    $res['status'] = 200;
+    $res['msg'] = "Data berhasil diinsert";
+    $res['body']['data']['kode'] = $kode;
+    $res['body']['data']['nama'] = $nama;
 } else {
-  $res['status'] = 400;
-  $res['msg'] = "Gagal membuat merek";
-  $res['body']="";
+    $res['status'] = 400;
+    $res['msg'] = "Gagal membuat merek";
+    $res['body']['data']['kode'] = $kode;
+    $res['body']['data']['nama'] = $nama;
 }
 
 echo json_encode($res);
