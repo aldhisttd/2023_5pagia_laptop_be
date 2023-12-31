c1dca0b2ce1138d7396f54bb74988b24a66fbf29
Fazamazaya <fitritriee02@gmail.com>
2023-12-31 16:21:55 +0700

    revisi B11 API Input Laptop

--- laptop_input.php
+++ laptop_input.php
@@ -1,6 +1,14 @@
 <?php
 include 'env.php';
 
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
 $kode = $_POST['kode'];
 $nama = $_POST['nama'];
 $kode_kategori = $_POST['kode_kategori'];
@@ -8,53 +16,35 @@ $kode_merek = $_POST['kode_merek'];
 $harga = $_POST['harga'];
 $deskripsi = $_POST['deskripsi'];
 
+// Proses upload file
+$temp = explode(".", $_FILES["gambar"]["name"]);
+$namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
+$target_file = "upload/" . $namaGambarBaru;
+move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
+// Set nama file gambar
+$gambar = 'upload/'.$namaGambarBaru;
+
 // cek duplikat
 $query = mysqli_query($koneksi, "SELECT * FROM laptop WHERE kode = '$kode'");
 $row = mysqli_num_rows($query);
 
 if($row == 1){
-    http_response_code(400);
-    $response = array(
-        "status" => 400,
-        "msg" => "Proses insert gagal",
-        "body" => array(
-            "data" => array(
-                "kode" => $kode,
-                "nama" => $nama,
-                "kode_kategori" => $kode_kategori,
-                "kode_merek" => $kode_merek,
-                "gambar" => array_key_exists("gambar", $_FILES) ? $_FILES["gambar"]["name"] : 'upload/gambar.jpg',
-                "harga" => $harga,
-                "deskripsi" => $deskripsi
-            )
-        )
-    );
+    $response['status'] = 400;
+    $response['msg'] = 'Proses insert gagal';
 } else {
-    $namaGambarBaru = 'gambar.jpg';
-    if (array_key_exists("gambar", $_FILES)) {
-        $temp = explode(".", $_FILES["gambar"]["name"]);
-        $namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
-        $target_file = "upload/" . $namaGambarBaru;
-        move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
-    }
 
-    mysqli_query($koneksi, "INSERT INTO laptop (kode, nama, gambar, kode_kategori, kode_merek, harga, deskripsi) VALUES ('$kode', '$nama', 'upload/$namaGambarBaru', '$kode_kategori', '$kode_merek', '$harga', '$deskripsi')");
+    $response['status'] = 200;
+    $response['msg'] = 'Data berhasil di insert';
+    $response['body']['data']['kode'] = $kode;
+    $response['body']['data']['nama'] = $nama;
+    $response['body']['data']['kode_kategori'] = $kode_kategori;
+    $response['body']['data']['kode_merek'] = $kode_merek;
+    $response['body']['data']['gambar'] = $gambar;
+    $response['body']['data']['harga'] = $harga;
+    $response['body']['data']['deskripsi'] = $deskripsi;
 
-    $response = array(
-        "status" => 200,
-        "msg" => "Data berhasil di insert",
-        "body" => array(
-            "data" => array(
-                "kode" => $kode,
-                "nama" => $nama,
-                "kode_kategori" => $kode_kategori,
-                "kode_merek" => $kode_merek,
-                "gambar" => 'upload/'.$namaGambarBaru,
-                "harga" => $harga,
-                "deskripsi" => $deskripsi
-            )
-        )
-    );
+    mysqli_query($koneksi, "INSERT INTO laptop (kode, nama, kode_kategori, kode_merek, gambar, harga, deskripsi) VALUES ('$kode', '$nama', '$kode_kategori', '$kode_merek', '$gambar', '$harga', '$deskripsi')");
 }
 
 echo json_encode($response);
