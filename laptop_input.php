45fc153bbd4be48e5d178efbdafd4ad44d7da1fb
Fazamazaya <fitritriee02@gmail.com>
2024-01-01 22:21:46 +0700

    revisi flow code B11 API Input Laptop

--- laptop_input.php
+++ laptop_input.php
@@ -16,15 +16,6 @@ $kode_merek = $_POST['kode_merek'];
 $harga = $_POST['harga'];
 $deskripsi = $_POST['deskripsi'];
 
-// Proses upload file
-$temp = explode(".", $_FILES["gambar"]["name"]);
-$namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
-$target_file = "upload/" . $namaGambarBaru;
-move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
-
-// Set nama file gambar
-$gambar = 'upload/'.$namaGambarBaru;
-
 // cek duplikat
 $query = mysqli_query($koneksi, "SELECT * FROM laptop WHERE kode = '$kode'");
 $row = mysqli_num_rows($query);
@@ -33,6 +24,11 @@ if($row == 1){
     $response['status'] = 400;
     $response['msg'] = 'Proses insert gagal';
 } else {
+    // Proses upload file
+    $temp = explode(".", $_FILES["gambar"]["name"]);
+    $namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
+    $target_file = "upload/" . $namaGambarBaru;
+    move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
 
     $response['status'] = 200;
     $response['msg'] = 'Data berhasil di insert';
@@ -40,11 +36,13 @@ if($row == 1){
     $response['body']['data']['nama'] = $nama;
     $response['body']['data']['kode_kategori'] = $kode_kategori;
     $response['body']['data']['kode_merek'] = $kode_merek;
-    $response['body']['data']['gambar'] = $gambar;
+    $response['body']['data']['gambar'] = 'upload/'.$namaGambarBaru;
     $response['body']['data']['harga'] = $harga;
     $response['body']['data']['deskripsi'] = $deskripsi;
 
-    mysqli_query($koneksi, "INSERT INTO laptop (kode, nama, kode_kategori, kode_merek, gambar, harga, deskripsi) VALUES ('$kode', '$nama', '$kode_kategori', '$kode_merek', '$gambar', '$harga', '$deskripsi')");
+    // Insert ke database
+    mysqli_query($koneksi, "INSERT INTO laptop (kode, nama, kode_kategori, kode_merek, gambar, harga, deskripsi) VALUES ('$kode', '$nama', '$kode_kategori', '$kode_merek', 'upload/$namaGambarBaru', '$harga', '$deskripsi')");
+
 }
 
 echo json_encode($response);
