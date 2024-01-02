d860d8728c352d5d2f7e162c99cead49a065e508
yesayas17 <yossiyesayas@gmail.com>
2024-01-02 16:02:59 +0700

    Done-B14 API Delete Laptop (Delete)

--- laptop_delete.php
+++ laptop_delete.php
@@ -21,11 +21,11 @@ $q = mysqli_query($koneksi, "DELETE FROM laptop WHERE kode='$kode'");
 if ($q) {
     $res['status'] = 200;
     $res['msg'] = "Data berhasil dihapus";
-    $res['body']['data']['kode'] = "$kode";
+    $res['body']['data']['kode'] = $kode;
 } else {
     $res['status'] = 400;
     $res['msg'] = "Data gagal dihapus";
-    $res['body']['data']['kode'] = "$kode";
+    $res['body']['data']['kode'] = $kode;
 }
 
 echo json_encode($res);
