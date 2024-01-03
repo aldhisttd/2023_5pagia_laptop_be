aaf359a990c73786c52f710e745fd513d39b392d
srningsih <srirahayuningsih24062002@gmail.com>
2024-01-03 08:16:50 +0700

    B13 API Data By Id Laptop (Read By ID)

--- laptop_read_one.php
+++ laptop_read_one.php
@@ -2,39 +2,36 @@
 include "env.php";
 
 if (isset($_REQUEST['kode'])) {
-    $kode = mysqli_real_escape_string($koneksi, $_REQUEST['kode']);
+    $kode = $_REQUEST['kode'];
 
     $query = "SELECT * FROM laptop WHERE kode='$kode'";
     $result = mysqli_query($koneksi, $query);
+    $laptopData = mysqli_fetch_assoc($result);
 
-    if ($result) {
-        $laptopData = mysqli_fetch_assoc($result);
-
-        if ($laptopData) {
-            $response = [
-                'status' => 200,
-                'msg' => 'success',
-                'body' => [
-                    'data' => [
-                        'kode' => $laptopData['kode'],
-                        'nama' => $laptopData['nama'],
-                        'kode_kategori' => $laptopData['kode_kategori'],
-                        'kode_merek' => $laptopData['kode_merek'],
-                        'gambar' => $laptopData['gambar'],
-                        'harga' => $laptopData['harga'],
-                        'deskripsi' => $laptopData['deskripsi']
-                    ]
+    if ($laptopData) {
+        $response = [
+            'status' => 200,
+            'msg' => 'success',
+            'body' => [
+                'data' => [
+                    'kode' => $laptopData['kode'],
+                    'nama' => $laptopData['nama'],
+                    'kode_kategori' => $laptopData['kode_kategori'],
+                    'kode_merek' => $laptopData['kode_merek'],
+                    'gambar' => $laptopData['gambar'],
+                    'harga' => $laptopData['harga'],
+                    'deskripsi' => $laptopData['deskripsi']
                 ]
-            ];
-        } else {
-            $response = [
-                'status' => 400,
-                'msg' => 'error',
-                'body' => [
-                    'data' => []
-                ]
-            ];
-        }
+            ]
+        ];
+    } else {
+        $response = [
+            'status' => 404,
+            'msg' => 'not found',
+            'body' => [
+                'data' => []
+            ]
+        ];
     }
 }
 
