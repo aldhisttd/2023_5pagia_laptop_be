a82bb652f0d8fc22028660225c2ac1203eee13c7
Aldhi <aldhi.albadri@gmail.com>
2024-01-17 10:23:39 +0700

    fix

--- laptop_read.php
+++ laptop_read.php
@@ -16,7 +16,14 @@ if (!isset($koneksi)) {
     $response['body']['data'] = [];
 } else {
 
-    $result = mysqli_query($koneksi, "SELECT * FROM laptop");
+    $q = "
+        SELECT A.*, B.nama as nama_kategori, C.nama as nama_merek
+        FROM laptop A, kategori B, merek C 
+        WHERE A.kode_kategori = B.kode 
+        AND A.kode_merek = C.kode;
+    ";
+    $result = mysqli_query($koneksi, $q);
+
     $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
     $response['status'] = 200;
